a76053191becda4bd85a2a1e144ea10655e2f4a0
Luca Brügger <lbruegger@puzzle.ch>
2022-02-15 15:48:53 +0100

    Begin with file entry

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_085536) do
+ActiveRecord::Schema.define(version: 2022_02_14_135340) do
 
   create_table "encryptables", force: :cascade do |t|
     t.string "name", limit: 70, default: "", null: false
